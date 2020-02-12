7aadb325e6992bb7df4ae709b09d98457523afbb
Helin Li <57208483+helinlee2010@users.noreply.github.com>
2020-02-11 21:33:41 -0800

    Create 1.Two Sum.java

---
+++ 1.Two Sum.java
@@ -0,0 +1,34 @@
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+
+mport java.util.HashMap;
+import java.util.Map;
+
+//Using hash map to solve
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+	    int [] input = new int[] {1,7,11,15};
+	    int target= 9;
+	    int[] result = twoSum(input, target);
+	    System.out.println("[" + result[0] +"," +result[1] + "]");
+    }
+
+    public static int[] twoSum(int[] arr, int target) throws Exception {
+        //Create a hash map indicates the visited numbers and their indices
+        Map<Integer, Integer> visitedNum = new HashMap<>();
+        for (int i=0; i<arr.length; i++){
+            int delta = target - arr[i];
+            if(visitedNum.containsKey(delta)){
+                return new int[] {i, visitedNum.getOrDefault(delta, -1)};
+            }
+            visitedNum.put(arr[i],i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
