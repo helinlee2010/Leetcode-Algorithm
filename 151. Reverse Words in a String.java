9591c14eef4ffe411335be1978f36ffbf19edc41
Helin Li <57208483+helinlee2010@users.noreply.github.com>
2020-02-11 22:31:23 -0800

    Update 151. Reverse Words in a String.java

--- 151. Reverse Words in a String.java
+++ 151. Reverse Words in a String.java
@@ -20,6 +20,11 @@ Note:
 A word is defined as a sequence of non-space characters.
 Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces. You need to reduce multiple spaces between two words to a single space in the reversed string.
 
+	
+//Reference(String Methods)
+// charAt(), trim()
+//Stringbuilder instance method: .toString() , .append()
+
 public class Solution {
 
     public static void main(String[] args) throws Exception {
