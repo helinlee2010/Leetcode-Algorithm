36cbf74d15cc529a1bf4fafa65fd842b315f7323
Helin Li <57208483+helinlee2010@users.noreply.github.com>
2020-06-15 18:44:11 -0700

    Update 937. Reorder Data in Log Files.java

--- 937. Reorder Data in Log Files.java
+++ 937. Reorder Data in Log Files.java
@@ -8,7 +8,6 @@ Each word after the identifier will consist only of digits.
 We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
 
 Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
-
 Return the final order of the logs.
 
  
