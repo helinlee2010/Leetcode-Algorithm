4e7cc0838246b49889025d43dadba45ab2eccb84
Helin Li <57208483+helinlee2010@users.noreply.github.com>
2020-08-12 14:14:09 -0700

    Update 104. Maximum Depth of Binary Tree.java

--- 104. Maximum Depth of Binary Tree.java
+++ 104. Maximum Depth of Binary Tree.java
@@ -13,6 +13,15 @@
  *     }
  * }
  */
+
+// Recursion, DFS, fast
+class Solution {
+    public int maxDepth(TreeNode root){
+        if(root==null) {return 0;}
+        int depth = Math.max(maxDepth(root.left)+1, maxDepth(root.right)+1);
+        return depth;
+    }
+}
  
 // BFS slower less memory
 class Solution {
