1a930d79c008d43347719553fd242c309999cbce
Helin Li <57208483+helinlee2010@users.noreply.github.com>
2020-10-10 00:36:14 -0700

    Update 79. Word Search.java

--- 79. Word Search.java
+++ 79. Word Search.java
@@ -13,32 +13,32 @@ Given word = "SEE", return true.
 Given word = "ABCB", return false.
   
   
-  public boolean exist(char[][] board, String word) {
-	        char firstChar = word.charAt(0);
-	        for(int i=0; i<board.length; i++){
-	                for(int j=0; j<board[0].length; j++){
-	                        if(dfs(i,j, board, word, 0)){
-	                                return true;
-	                        }
-	                }
-	        }
-	        return false;
+public boolean exist(char[][] board, String word) {
+	char firstChar = word.charAt(0);
+	for(int i=0; i<board.length; i++){
+		for(int j=0; j<board[0].length; j++){
+			if(dfs(i,j, board, word, 0)){
+				return true;
+			}
+		}
 	}
+	return false;
+}
 	
-	private boolean dfs(int i, int j, char[][] board, String word, int idx){
-	        if(idx == word.length()) return true; 
-	        
-	        if(i<0 || i>= board.length || j<0 || j>= board[0].length || board[i][j] != word.charAt(idx)) return false;
-	        
-	        char temp = board [i][j];
-	        board[i][j] = '*';
-	        
-	        // Left, Down, Right, Up
-	        boolean found = dfs(i, j-1, board, word, idx+1) 
-	                || dfs(i+1, j, board, word, idx+1)
-	                || dfs(i, j+1, board, word, idx+1) 
-	                || dfs(i-1, j, board, word, idx+1);
-	        
-	        board[i][j] = temp;         // Important !
-	        return found;
-	}
+private boolean dfs(int i, int j, char[][] board, String word, int idx){
+	if(idx == word.length()) return true; 
+
+	if(i<0 || i>= board.length || j<0 || j>= board[0].length || board[i][j] != word.charAt(idx)) return false;
+
+	char temp = board [i][j];
+	board[i][j] = '*';
+
+	// Left, Down, Right, Up
+	boolean found = dfs(i, j-1, board, word, idx+1) 
+		|| dfs(i+1, j, board, word, idx+1)
+		|| dfs(i, j+1, board, word, idx+1) 
+		|| dfs(i-1, j, board, word, idx+1);
+
+	board[i][j] = temp;         // Important !
+	return found;
+}
