097434130d9234cb853554242b80e5ef86c571a3
Helin Li <57208483+helinlee2010@users.noreply.github.com>
2020-10-13 00:00:51 -0700

    Update 74. Search a 2D matrix.java

--- 74. Search a 2D matrix.java
+++ 74. Search a 2D matrix.java
@@ -1,3 +1,14 @@
+https://leetcode.com/problems/search-a-2d-matrix/ 
+Write an efficient algorithm that searches for a value in an r*c matrix. This matrix has the following properties:
+• Integers in each row are sorted from left to right.
+• The first integer of each row is greater than the last integer of the previous row.
+
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 3
+Output: true
+	
+// Approach: Double Binary Search 
+// Time: log(r)+log(c)
+
 public boolean searchMatrix(int[][] matrix, int target) {
 	// Corner case checking
 	if(matrix.length == 0 || matrix[0].length == 0 ) return false;
