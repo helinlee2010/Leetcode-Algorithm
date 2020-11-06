cefecb2af8953b5358f90eaee3bcab9425009237
Helin Li <57208483+helinlee2010@users.noreply.github.com>
2020-11-06 11:51:37 -0800

    Update 221. Maximal Square.java

--- 221. Maximal Square.java
+++ 221. Maximal Square.java
@@ -13,27 +13,27 @@ Output: 4
 // Time&Space: O(M*N)
 
 public int maximalSquare(char[][] matrix) {
-	        // Corner case;
-	        if(matrix.length == 0) return 0;
-	        
-	        int row = matrix.length;
-	        int col = matrix[0].length;
-	        
-	        int maxWidth = 0;
-	        
-	        int[][] dp = new int[row][col];
-	        
-	        for(int r=0; r<row; r++){
-	                for(int c=0; c<col; c++){
-	                        if(matrix[r][c]=='1'){
-	                                if(r==0 || c==0){
-	                                        dp[r][c] = 1;
-	                                }else{
-	                                        dp[r][c] = Math.min(dp[r][c-1], Math.min(dp[r-1][c],dp[r-1][c-1])) + 1;
-	                                }
-	                                maxWidth = Math.max(maxWidth, dp[r][c]);
-	                        }
-	                }
-	        }
-	        return maxWidth*maxWidth;
+	// Corner case;
+	if(matrix.length == 0) return 0;
+
+	int row = matrix.length;
+	int col = matrix[0].length;
+
+	int maxWidth = 0;
+
+	int[][] dp = new int[row][col];
+
+	for(int r=0; r<row; r++){
+		for(int c=0; c<col; c++){
+			if(matrix[r][c]=='1'){
+				if(r==0 || c==0){
+					dp[r][c] = 1;
+				}else{
+					dp[r][c] = Math.min(dp[r][c-1], Math.min(dp[r-1][c],dp[r-1][c-1])) + 1;
+				}
+				maxWidth = Math.max(maxWidth, dp[r][c]);
+			}
+		}
 	}
+	return maxWidth*maxWidth;
+}
