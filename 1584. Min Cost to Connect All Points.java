3f49dd219ea602647b42eb297181c0ba7b8daeb2
Helin Li <57208483+helinlee2010@users.noreply.github.com>
2021-01-07 01:16:22 -0800

    Create 1584. Min Cost to Connect All Points.java

---
+++ 1584. Min Cost to Connect All Points.java
@@ -0,0 +1,46 @@
+
+// Prim's Algo for Minimum Spanning Tree (MST)
+// Alternatively, can use Kruskal's Algo for MST
+
+	public int minCostConnectPoints(int[][] points) {
+	        int n = points.length;
+	        boolean[] visited = new boolean[n];
+	        
+	        // Start with point 0
+	        visited[0] = true;
+	        
+	        // Create PQ/minHeap based on distance
+	        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
+	        // Dummy
+	        // pq.offer(new int[]{0,0});
+	        
+	        // Init result variable
+	        int cost = 0;
+	        // Start with point 0
+	        int curr = 0;
+	        int connected = 1;
+	        
+	        // increment before evaluating
+	        while(++connected<=n) {
+	                
+	                // Loop through this point's neighbors
+	                for (int k = 1; k < n; k++) {
+	                        
+	                        if (!visited[k]) {
+	                                // PQ storing: [distFromCurrPoint, endPointIdx]
+	                                pq.add(new int[]{Math.abs(points[k][0] - points[curr][0]) + Math.abs(points[k][1] - points[curr][1]), k});
+	                        }
+	                }
+	                
+	                while (visited[pq.peek()[1]]){
+	                        pq.poll();
+	                }
+	                
+	                // if(!pq.isEmpty())
+	                int[] next = pq.poll();
+	                visited[next[1]] = true;
+	                curr = next[1];
+	                cost += next[0];
+	        }
+	        return cost;
+	}
